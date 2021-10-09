f96850e27c129b83fac873077e6d42876b0b60a0
Alex Losev <cahektpx1990@gmail.com>
2021-10-09 12:57:03 +0300

    lesson06 done

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 "use strict";
+let num;
+let randomNum;
 
+
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const getRandomNumber = function(maxNum) {
+  return Math.floor(Math.random() * Math.floor(maxNum));
+};
+
+
+const startGame = function() {
+
+  randomNum = getRandomNumber(100);
+  console.log(randomNum);
+
+  const game = function() {
+    num = prompt("Угадай число от 1 до 100");
+    if(num === null) {
+      alert("Игра окончена");
+      return;
+    }
+    if (isNumber(num)) {
+      num = +num;
+      if (num === randomNum) {
+        alert('Поздравляю, Вы угадали!!!');
+        return;
+      } else if (num > randomNum) {
+        alert('Загаданное число меньше');
+        game();
+      } else if (num < 100) {
+        alert("Загаданное число больше");
+        game();
+      } 
+    } else {
+      alert("Введи число!");
+      game();
+    }
+  };
+  game();
+};
+
+startGame();
